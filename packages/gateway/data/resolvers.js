4322ead7e5d83f217787fd2b5850f89f9c2f45a7
flosloot <flosloot@gmx.de>
2018-06-09 14:36:36 +0200

    clean up schema and resolvers

--- packages/gateway/data/resolvers.js
+++ packages/gateway/data/resolvers.js
@@ -1,5 +1,38 @@
+// type Mail {
+//   subject: String
+//   receiver: String
+//   content: String
+//   _id: String
+// }
+
+const mockMails = [
+  {
+    subject: 'My first Email',
+    receiver: 'test@test.com',
+    content: 'hello world'
+  },
+  {
+    subject: 'My second Email',
+    receiver: 'test@test.com',
+    content: 'hello world'
+  },
+  {
+    subject: 'My third Email',
+    receiver: 'test@test.com',
+    content: 'hello world'
+  }
+];
+
 module.exports = {
   Query: {
-    hey: () => 'hey you!!'
+    mails: () => mockMails,
+    mail: (_, args) => mockMails[0]
+  },
+  Mutation: {
+    mail: (_, args) => {
+      mockMails[0] = args;
+
+      return args;
+    }
   }
 };
--- packages/gateway/data/schema.js
+++ packages/gateway/data/schema.js
@@ -2,7 +2,21 @@ const { makeExecutableSchema } = require('graphql-tools');
 const resolvers = require('./resolvers');
 
 const typeDefs = `
-  type Query { hey: String! }
+  type Query {
+    mails: [Mail]
+    mail(subject: String!, receiver: String!): Mail
+  }
+
+  type Mutation {
+    mail(subject: String!, receiver: String!, content: String!): Mail
+  }
+
+  type Mail {
+    subject: String
+    receiver: String
+    content: String
+    _id: String
+  }
 `;
 
 module.exports = makeExecutableSchema({ typeDefs, resolvers });
